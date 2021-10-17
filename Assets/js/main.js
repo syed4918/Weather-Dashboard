7dd1aedc130faf5a629e769c496f11103b451a96
Syed Jamil <syed.jamil13@gmail.com>
2021-10-16 23:27:01 -0400

    5-day weather forecast now displays on dashboard and search history shows most recent cities

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -8,10 +8,54 @@ const infoHumid = document.getElementById("Humidity");
 const infoUV = document.getElementById("UV");
 
 var currentTime = moment();
-console.log(currentTime);
+const apiKey = "";
 
+let cityHistory = [];
+if (localStorage.getItem("cityHistory") != null) {
+    cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+}
 
-const apiKey = "d70ef8022f47b07da7d017d3bf99f53b";
+function saveHistory() {
+    localStorage.setItem("cityHistory", JSON.stringify(cityHistory)); 
+}
+
+function fillHistory(){
+    searchHistory.innerHTML = "";
+    console.log(cityHistory.length);
+    for(i = 0; i < cityHistory.length; i++){
+        var searchedCity = document.createElement("button");
+        searchedCity.textContent = cityHistory[i];
+        searchHistory.appendChild(searchedCity);
+    }
+}
+
+function searchCity(){
+    getCity(submitText.value);
+}
+
+function addToHistory(cityName){
+    console.log(cityName + " " + cityHistory.length);
+    let noMatch = true;
+    for(i = 0; i < cityHistory.length; i++){
+        console.log(cityHistory[i] + " " + cityName.value);
+        if(cityHistory[i].toUpperCase() === cityName.toUpperCase()){
+            noMatch = false;
+        }
+    }
+    if(noMatch){
+        console.log("noMatch");
+        if(cityHistory.length >= 10){
+            cityHistory.splice(0, 1);
+        }
+        console.log(cityHistory);
+        cityHistory.push(cityName.toUpperCase());
+        console.log(cityHistory);
+        saveHistory();
+        fillHistory();
+    }
+    submitText.textContent = "";
+    submitText.TEXT_NODE = "";
+}
 
 function getCity(city){
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
@@ -22,6 +66,7 @@ function getCity(city){
     })
     .catch(function() {
       // catch any errors
+      return null;
     });
 }
 
@@ -38,11 +83,51 @@ function addCity(city){
     .then(function (response) {
       return response.json();
     })
+
     .then(function (data) {
-        console.log(data);
         infoUV.textContent = "UV Index: " + data.current.uvi;
-    })
-    console.log(city);
+        if(data.current.uvi < 2){
+          infoUV.setAttribute("class", "goodUV");
+      } else if(data.current.uvi < 7){
+          infoUV.setAtrribute("class", "moderateUV");
+      } else {
+          infoUV.setAttribute("class", "badUV");
+      }
+      fiveDayInfo.innerHTML = "";
+      for(i =1; i < 6; i++){
+          let weekDay = data.daily[i];
+          let dayDate = document.createElement("div");
+          dayDate.textContent = moment(new Date(data.daily[i].dt * 1000)).format("MM/DD/YYYY");
+
+          let dayIcon = document.createElement("img");
+          dayIcon.style.width = "40px";
+          dayIcon.style.height = "40px";
+          dayIcon.setAttribute("src", "http://openweathermap.org/img/w/" + weekDay.weather[0].icon + ".png");
+          let dayTemp = document.createElement("p");
+          dayTemp.textContent = "Temp: " + weekDay.temp.day + "°F";
+          let dayWind = document.createElement("p");
+          dayWind.textContent  = "Wind: " + weekDay.wind_speed + " MPH";
+          let dayHumid = document.createElement("p");
+          dayHumid.textContent = "Humidity: " + weekDay.humidity + " %";
+
+          let infoBox = document.createElement("div");
+          fiveDayInfo.appendChild(infoBox);
+          infoBox.appendChild(dayDate);
+          infoBox.appendChild(dayIcon);
+          infoBox.appendChild(dayTemp);
+          infoBox.appendChild(dayWind);
+          infoBox.appendChild(dayHumid);
+      }
+  })
 }
+
+function showFromHistory(event) {
+  var element = event.target;
+  if(element.nodeName === "BUTTON"){
+      getCity(element.textContent);
+  }
+}
+
+fillHistory();
 
 getCity("Philadelphia");
