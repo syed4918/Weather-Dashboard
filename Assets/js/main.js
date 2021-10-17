7e4e1ef62da50cfd80d1bc4e6f14dc246da8fb7a
Syed Jamil <syed.jamil13@gmail.com>
2021-10-16 23:06:30 -0400

    Updated main.js to display respective weather info and finishing touches on style.css

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,15 +1,48 @@
-var apiKey = "";
+const submitText = document.getElementById("submitText");
+const submitButton = document.getElementById("submitButton");
+const infoName = document.getElementById("infoName");
+const weatherIcon = document.getElementById("icon");
+const infoTemp = document.getElementById("Temperature");
+const infoWind = document.getElementById("Wind");
+const infoHumid = document.getElementById("Humidity");
+const infoUV = document.getElementById("UV");
+
+var currentTime = moment();
+console.log(currentTime);
+
+
+const apiKey = "d70ef8022f47b07da7d017d3bf99f53b";
 
 function getCity(city){
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
     fetch(queryURL)
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
-      console.log(data);
+      addCity(data);
     })
     .catch(function() {
       // catch any errors
     });
 }
 
-getCity("Pittsburgh");
+function addCity(city){
+    infoName.textContent = city.name + " " + moment().format("L");
+    weatherIcon.setAttribute("src", "http://openweathermap.org/img/w/" + city.weather[0].icon + ".png")
+    infoTemp.textContent = "Temp: " + Math.floor((city.main.temp - 273.15) * 9/5 + 32) + "°F";
+    infoWind.textContent = "Wind: " + city.wind.speed + "MPH";
+    infoHumid.textContent = "Humidity: " + city.main.humidity + " %";
+    let lat = city.coord.lat;
+    let lon = city.coord.lon;
+    let uvKey = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid="+apiKey;
+    fetch(uvKey)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        infoUV.textContent = "UV Index: " + data.current.uvi;
+    })
+    console.log(city);
+}
+
+getCity("Philadelphia");
