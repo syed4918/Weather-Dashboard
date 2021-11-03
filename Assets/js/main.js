3f62b9b3b3130ad9d74525e7772859a7d95e5796
Syed Jamil <syed.jamil13@gmail.com>
2021-11-03 12:29:54 -0400

    Add commentary to JS and add heading to each 5-day section

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,3 +1,24 @@
-var searchButton = $('#searchBtn');
-var history = $('#history');
-var today = moment();
+// Varaiable declarations for required weather info
+// for the city that the user searches for
+const searchButton = $('#searchBtn'); // search button would need a click event 
+const apiKey = "" ; // to get the weather info using the given weather API
+var city = $('#city') // city that the user wants the weather for
+const currentWeather = $('#currentWeather'); // container holding current weather info
+
+// these are part of the currentWeather container
+var today = moment(); // display the current date  
+var tempNow = $('#temp'); // temperature for that city
+var humidNow = $('#humid'); // humdity for that city
+var uviNow = $('#uvi'); // UV index for that city
+
+// separate from currentWeather 
+var history = $('#history'); 
+// grab search history based on local storage (needs to add it if empty)
+// and display current and future conditions 
+// when that city is clicked 
+
+// 5-day Weather forecast section
+const future = $('#5-day');
+ // 5-day forecast would display current + next 4 dates 
+// along with the temperature, date, humidity, and weather icon respecitive to each day
+
