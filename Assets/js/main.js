6e19446577d2baac8cac0289d6013467e8b48dba
Syed Jamil <syed.jamil13@gmail.com>
2021-11-04 15:56:04 -0400

    Add additional commentary to JS and begin button click event

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,7 +1,7 @@
 // Varaiable declarations for required weather info
 // for the city that the user searches for
 const searchButton = $('#searchBtn'); // search button would need a click event 
-const apiKey = "" ; // to get the weather info using the given weather API
+const apiKey = ""; // to get the weather info using the given weather API
 var city = $('#city') // city that the user wants the weather for
 const currentWeather = $('#currentWeather'); // container holding current weather info
 
@@ -11,14 +11,48 @@ var tempNow = $('#temp'); // temperature for that city
 var humidNow = $('#humid'); // humdity for that city
 var uviNow = $('#uvi'); // UV index for that city
 
-// separate from currentWeather 
-var history = $('#history'); 
-// grab search history based on local storage (needs to add it if empty)
-// and display current and future conditions 
-// when that city is clicked 
+function weatherSearch() { 
+    //use api key to retrieve info for the current day 
+    //and use openWeather API documentation to find required syntax for the following:
+    // temperature 
+    // wind speed 
+    // humidity
 
-// 5-day Weather forecast section
+    // activity 4 - week 6 for fectch and .then responses 
+    console.log("search button clicked")
+
+    // when search button is clicked
+    //searchButton.addEventListener('click', weatherSearch);
+    // performs the above tasks when the button search button is clicked 
+    $("#searchBtn").click(weatherSearch); // needed JQuery version as vanilla JS is giving an issue
+}
+
+// 5-day Weather forecast section for the city searched 
 const future = $('#5-day');
- // 5-day forecast would display current + next 4 dates 
-// along with the temperature, date, humidity, and weather icon respecitive to each day
+// 5-day forecast would display current + next 4 dates 
+// along with the temperature, date, humidity, and weather icon respective to each day
+
+// history section
+var history = $('#history');
+
+// create empty array to store previously searched cities into local storage
+let searchedCities = []; // empty array
+function weatherHistory() { // history function 
+    // saves the the empty array to local storage and updates as more cities are searched 
+    localStorage.setItem('searchedCities', JSON.stringify('searchedCities'));
+
+    // append each city to the array in local storage if it exists (but has no values inside of it)
+    if (localStorage.getItem('searchedCities') != null) {
+        searchedCities = JSON.parse(localStorage.getItem("searchedCities"));
+    }
+
+    // display current and future conditions 
+    // including the date, icon, temperature, wind speed, humidity
+    // when that city is clicked 
+}
+
+
+
+
+
 
